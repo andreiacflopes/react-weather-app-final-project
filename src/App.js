a22e3b498647405b4275f1ec0b9aa97377a22711
andreiacflopes <andreiacflopes@gmail.com>
2021-02-21 16:05:32 +0000

    added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -4,12 +4,10 @@ import Weather from "./Weather";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>
-          Hello world!
-        </h1>
-        <Weather />
-      </header>
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by <a href="www.linkedin.com/in/andreiacflopes" target="_blank">Andreia Lopes</a> and is <a href="https://github.com/andreiacflopes/react-weather-app-final-project" target="_blank">open-sourced on GitHub</a>
+      </footer>
     </div>
   );
 }
