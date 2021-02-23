fb7c6353752ec48c2cd496ac526fa6b74a54c446
andreiacflopes <andreiacflopes@gmail.com>
2021-02-23 13:39:39 +0000

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import Weather from "./Weather";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by <a href="www.linkedin.com/in/andreiacflopes" target="_blank">Andreia Lopes</a> and is <a href="https://github.com/andreiacflopes/react-weather-app-final-project" target="_blank">open-sourced on GitHub</a>
-      </footer>
+        <div className="container">
+          <h1>Weather App</h1>
+          <footer>
+            This project was coded by <a href="www.linkedin.com/in/andreiacflopes" target="_blank">Andreia Lopes</a> and is <a href="https://github.com/andreiacflopes/react-weather-app-final-project" target="_blank">open-sourced on GitHub</a>
+          </footer>
+        </div>
     </div>
   );
 }
